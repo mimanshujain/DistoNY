cb0ea0a904ac3415f43856633aa1ed9e8836d02a
mimanshujain <mimanshujain@gmail.com>
2015-10-12 14:46:55 -0400

    Final Draft V1

---
+++ distoUtils.js
@@ -0,0 +1,412 @@
+loadData("ny");
+
+function loadData(region){
+var eriemap = {};
+eriemap["id"] = "erie"
+eriemap["filename"] = "ErieCountyMainCSV.topojson";
+eriemap["scale"] = 33;
+eriemap["tarns1"] = -23600;
+eriemap["tarns2"] = -4750;
+
+var nymap = {};
+nymap["id"] = "ny"
+nymap["filename"] = "CountyDataNewCSVMerge.topojson";
+nymap["scale"] = 6.5;
+nymap["tarns1"] = -4600;
+nymap["tarns2"] = -640;
+
+var allmaps = {};
+allmaps["erie"] = eriemap;
+allmaps["ny"] = nymap;
+// //console.log(allmaps["erie"]);
+
+var datamap = allmaps[region];
+var width = "30%",
+    height = 590;
+
+if(datamap.id=="ny"){
+  document.getElementById("portfolio").innerHTML = "New York Distorted Map"
+  document.getElementById("top").style.display='none';
+  width = "65%"
+} else{
+  document.getElementById("portfolio").innerHTML = "Erie County Distorted Map"
+    document.getElementById("top").style.display='block';
+}
+
+// //console.log(datamap);
+
+var mapid = document.getElementById("mapc")
+if(mapid != undefined)
+  mapid.innerHTML = "";
+var svg1 = d3.select("#mapc").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("id", "map")
+    .attr("style","margin-left:auto; margin-right:auto; diaplay:block;");
+
+// hide the form if the browser doesn't do SVG,
+// (then just let everything else fail)
+if (!document.createElementNS) {
+  document.getElementsByTagName("form")[0].style.display = "none";
+}
+
+// field definitions from:
+// <http://www.census.gov/popest/data/national/totals/2011/files/NST-EST2011-alldata.pdf>
+var percent = (function() {
+      var fmt = d3.format(".2f");
+      return function(n) { return fmt(n) + "%"; };
+    })(),
+    fields = [
+      {name: "Default (Area (sq mi))", id: "default", key: "Muni Area (sq mi)", ratio:"none", ny:false, erie:true},
+      {name: "Default", id: "none", ny:true, erie:false},
+      {name: "Population", id: "Population", key: "Population", ratio:"none", ny:true,erie:true},
+      {name: "Population Per Square Mile", id: "Population Per Square Mile", key: "Population Per Square Mile", ratio:"none", ny:false,erie:true},
+/*      {name: "Area (sq mi)", id: "Muni Area (sq mi)", key: "Muni Area (sq mi)", ratio:"none"},
+*/      {name: "% Households With Cable", id: "%_HH_With_Cable", key: "%_HH_With_Cable", ratio:"none", ny:false,erie:true},
+      {name: "% Households With Fiber", id: "%_HH_With_Fiber", key: "%_HH_With_Fiber", ratio:"none", ny:false,erie:true},
+      {name: "Residential Market Value Ratio", id: "Residential Market Value Ratio", key: "Residential Market Value Ratio", ratio:"none", ny:false,erie:true},
+      {name: "Households per sq mi", id: "HH", key: "HH", ratio: "Muni Area (sq mi)", ny:false,erie:true},
+      {name: "Family Households per sq mi", id: "HH_fam", key: "HH_fam", ratio: "Muni Area (sq mi)", ny:true,erie:true},
+      {name: "Married Couple Families (%)", id: "Married", key: "Married", ratio:"Population", ny:false,erie:true},
+      {name: "Male (%)", id: "Male", key: "Male", ratio:"Population", ny:true,erie:true},
+      {name: "Female (%)", id: "Female", key: "Female", ratio:"Population", ny:true,erie:true},
+      {name: "One_Race (%)", id: "One_Race", key: "One_Race", ratio:"Population", ny:false,erie:true},
+      {name: "Black or African Americans (%)", id: "Black or AA", key: "Black or AA", ratio:"Population", ny:true,erie:true},
+      {name: "Whites (%)", id: "White", key: "White", ratio:"Population", ny:true,erie:true},
+      {name: "Asians (%)", id: "Asian", key: "Asian", ratio:"Population", ny:true,erie:true},
+      {name: "High School Grads (%)", id: "HSgrad", key: "HSgrad", ratio:"Population", ny:false,erie:true},
+      {name: "College (%)", id: "SomeCollege", key: "SomeCollege", format: "+,", ratio:"Population", ny:false,erie:true},
+      {name: "Associate Degree (%)", id: "Assoc_Degree", key: "Assoc_Degree", ratio:"Population", ny:false,erie:true},
+      {name: "Bach Degree (%)", id: "Bach_Degree", key: "Bach_Degree", ratio:"Population", ny:false,erie:true},
+      {name: "Grad_Prof_degree (%)", id: "Grad_Prof_degree", key: "Grad_Prof_degree", ratio:"Population", ny:false,erie:true},
+      {name: "Per_HSgradHigher (%)", id: "Per_HSgradHigher", key: "Per_HSgradHigher", ratio:"Population", ny:false,erie:true},
+      {name: "PerBachorHigher (%)", id: "PerBachorHigher", key: "PerBachorHigher", ratio:"Population", ny:false,erie:true},
+    ],
+    years = [2010, 2011],
+    fieldsById = d3.nest()
+      .key(function(d) { return d.id; })
+      .rollup(function(d) { return d[0]; })
+      .map(fields),
+    field = fields[0],
+    year = years[0],
+    colors = colorbrewer.RdYlBu[3]
+      .reverse()
+      .map(function(rgb) { return d3.hsl(rgb); });
+
+var body = d3.select("body"),
+    stat = d3.select("#status");
+
+var fieldSelect = d3.select("#feild")
+ 
+var filid = document.getElementById("field")
+if(filid != undefined)
+  filid.innerHTML = "";
+
+  ftable = document.getElementById("field");
+  var i=0;
+ for(var f in fields){
+    if(fields[f][datamap.id]){
+      var ele = document.createElement("a");
+      ele.setAttribute('href', "#"+fields[f].id);
+      ele.setAttribute('text', fields[f].id);
+      ele.setAttribute('class', "fieldele");
+      ele.innerHTML = fields[f].name;
+      var r = ftable.insertRow(i);
+      var c = r.insertCell(0);
+      c.appendChild(ele);
+      i++;
+    }
+ }
+
+var yearSelect = d3.select("#year")
+  .on("change", function(e) {
+    year = years[this.selectedIndex];
+    location.hash = "#" + [field.id, year].join("/");
+  });
+
+yearSelect.selectAll("option")
+  .data(years)
+  .enter()
+  .append("option")
+    .attr("value", function(y) { return y; })
+    .text(function(y) { return y; })
+
+var map = d3.select("#map"),
+    zoom = d3.behavior.zoom()
+      .translate([datamap.tarns1, datamap.tarns2 ])
+      .scale(datamap.scale) 
+      .scaleExtent([0.5, 10.0])
+      .on("zoom", updateZoom),
+    layer = map.append("g")
+      .attr("id", "layer"),
+    states = layer.append("g")
+      .attr("id", "states")
+      .selectAll("path");
+
+// map.call(zoom);
+updateZoom();
+
+function updateZoom() {
+  var scale = zoom.scale();
+  layer.attr("transform",
+    "translate(" + zoom.translate() + ") " +
+    "scale(" + [scale, scale] + ")");
+}
+var proj = d3.geo.albersUsa();
+   var topology,
+    geometries,
+    rawData,
+    dataById = {},
+    carto = d3.cartogram()
+      .projection(proj)
+      .properties(function(d) {
+        return dataById[d.id];
+      })
+      .value(function(d) {
+        return +d.properties[field];
+      });
+
+window.onhashchange = function() {
+  parseHash();
+};
+
+var segmentized = location.search === "?segmentized",
+/*          url = ["data","erienewcsv.topojson"].join("/");
+*/          url = ["http://localhost/data",datamap.filename].join("/");
+d3.json(url, function(topo) {
+  topology = topo;
+  geometries = topology.objects.states.geometries;
+  d3.csv("data/wny.csv", function(data) {
+    rawData = data;
+    dataById = d3.nest()
+      .key(function(d) { return d.NAME; })
+      .rollup(function(d) { return d[0]; })
+      .map(data);
+    init();
+  });
+});
+
+function init() {
+  var features = carto.features(topology, geometries),
+      path = d3.geo.path()
+        .projection(proj);
+
+  states = states.data(features)
+    .enter()
+    .append("path")
+      .attr("class", "state")
+      .attr("id", function(d) {
+        return d.properties.NAME;
+      })
+      .attr("fill", "#fafafa")
+      .attr("d", path);
+
+  
+  states.append("title");
+
+  parseHash();
+}
+
+function addTexts(){
+  var list = document.getElementsByClassName("state-label");
+   for(var i = list.length - 1; 0 <= i; i--)
+   if(list[i] && list[i].parentElement)
+   list[i].parentElement.removeChild(list[i]);
+
+  var paths = document.querySelectorAll("path");
+
+  for (var p in paths)
+  {
+      addText(paths[p])
+  }
+}
+
+function addText(p)
+{
+    var t = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    var b = p.getBBox();
+    t.setAttribute("transform", "translate(" + (b.x + b.width/2 ) + " " + (b.y + b.height/2) + ")");
+    t.textContent = p.id;
+    t.setAttribute("fill", "#777");
+    t.setAttribute("font-size", ".3");
+    t.setAttribute("class","state-label");
+    p.parentNode.insertBefore(t, p.nextSibling);
+}
+
+function reset() {
+  stat.text("");
+  body.classed("updating", false);
+
+  var features = carto.features(topology, geometries),
+      path = d3.geo.path()
+        .projection(proj);
+
+  states.data(features)
+    .transition()
+      .duration(1050)
+      .ease("linear")
+      .attr("fill", "#fafafa")
+      .attr("d", path);
+
+  states.select("title")
+    .text(function(d) {
+      return d.properties.NAME;
+    });
+}
+
+  function valueWithRatio(key, ratio, d){
+    if(ratio!="none")
+      return +parseFloat((+d.properties[key])/(+d.properties[ratio])).toFixed(3);
+    else
+      return +d.properties[key];
+  }
+
+
+function update() {
+  var tuples = [];
+
+
+  var start = Date.now();
+  body.classed("updating", true);
+  var formatter = d3.format(".2f");
+  var ratio = field.ratio;
+  var feildname = field.name;
+  var key = field.key.replace("%d", year),
+      fmt = (typeof field.format === "function")
+        ? field.format
+        : d3.format(field.format || ","),
+      value = function(d) {
+        tuples.push([d.id, valueWithRatio(key,ratio,d)]);
+        return valueWithRatio(key,ratio,d);
+      },
+      values = states.data()
+        .map(value)
+        .filter(function(n) {
+          return !isNaN(n);
+        })
+        .sort(d3.ascending),
+      lo = values[0],
+      hi = values[values.length - 1];
+      //console.log(values);
+
+  tuples.sort(function(a, b) {
+      a = a[1];
+      b = b[1];
+      return a < b ? 1 : (a > b ? -1 : 0);
+  });
+
+  toptable = document.getElementById("toptable");
+  toptable.innerHTML = "";
+  var r = toptable.insertRow(0);
+  var c = r.insertCell(0);
+  c.innerHTML = "<b>Top 10 in "+feildname+"</b>";
+
+  for (var i = 0; i < tuples.length && i<10; i++) {
+      //console.log(tuples[i][0]+"  "+tuples[i][1]);
+      var r = toptable.insertRow(i+1);
+      var c = r.insertCell(0);
+      c.innerHTML = (i+1)+". "+tuples[i][0]+ ": "+tuples[i][1];
+  }
+
+  var color = d3.scale.linear()
+    .range(colors)
+    .domain(lo < 0
+      ? [lo, 0, hi]
+      : [lo, d3.mean(values), hi]);
+
+  // normalize the scale to positive numbers
+  var scale = d3.scale.linear()
+    .domain([lo, hi])
+    .range([1, 1000]);
+
+  // tell the cartogram to use the scaled values
+  carto.value(function(d) {
+    return scale(value(d));
+  });
+
+  // generate the new features, pre-projected
+  var features = carto(topology, geometries).features;
+
+  // update the data
+  states.data(features)
+    .select("title")
+      .text(function(d) {
+        return [d.properties.NAME, fmt(value(d))].join(": ");
+      });
+
+  states.transition()
+    .duration(750)
+    .ease("linear")
+    .attr("fill", function(d) {
+      return color(value(d));
+    })
+    .attr("d", carto.path);
+
+  var delta = (Date.now() - start) / 1000;
+/*        stat.text(["calculated in", delta.toFixed(1), "seconds"].join(" "));
+*/        body.classed("updating", false);
+
+  //addTexts();
+  }
+
+var deferredUpdate = (function() {
+  var timeout;
+  return function() {
+    var args = arguments;
+    clearTimeout(timeout);
+/*          stat.text("calculating...");
+*/          return timeout = setTimeout(function() {
+      update.apply(null, arguments);
+    }, 10);
+  };
+})();
+
+var hashish = d3.selectAll("a.hashish")
+  .datum(function() {
+    return this.href;
+  });
+
+function parseHash() {
+  var parts = location.hash.substr(1).split("/"),
+      desiredFieldId = parts[0],
+      desiredYear = +parts[1];
+
+  field = fieldsById[desiredFieldId] || fields[0];
+  year = (years.indexOf(desiredYear) > -1) ? desiredYear : years[0];
+
+  fieldSelect.property("selectedIndex", fields.indexOf(field));
+
+  if (field.id === "none") {
+    toptable = document.getElementById("toptable");
+    toptable.innerHTML = "";
+    yearSelect.attr("disabled", "disabled");
+    reset();
+
+  } else {
+
+    if (field.years) {
+      if (field.years.indexOf(year) === -1) {
+        year = field.years[0];
+      }
+      yearSelect.selectAll("option")
+        .attr("disabled", function(y) {
+          return (field.years.indexOf(y) === -1) ? "disabled" : null;
+        });
+    } else {
+      yearSelect.selectAll("option")
+        .attr("disabled", null);
+    }
+
+    yearSelect
+      .property("selectedIndex", years.indexOf(year))
+      .attr("disabled", null);
+
+    deferredUpdate();
+    location.replace("#" + [field.id, year].join("/"));
+
+    hashish.attr("href", function(href) {
+      return href + location.hash;
+    });
+  }
+}  
+}
---
+++ run.py
@@ -0,0 +1,175 @@
+# coding: utf-8
+import cPickle
+import xlrd
+from flask import Flask, render_template, jsonify, request
+import os
+import numpy as np
+import re
+import simplejson
+# from bson.json_util import dumps
+import json
+# import requests
+app = Flask(__name__)
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+@app.route('/', methods=['GET'])
+def index():
+    path = os.getcwd()
+    return render_template(path+'/index.html')
+
+# @app.route('/bank', methods=['GET'])
+# def bank():
+# 	return render_template('bank.html')
+#
+# @app.route('/merchant', methods=['GET'])
+# def get_merchant():
+# 	r = requests.get("http://api.reimaginebanking.com/merchants?key=b3ecb3d19e91b8fa639d3f798dbbfaca")
+# 	return json.dumps(r.text)
+
+# @app.route('/get_user_purchase_info', methods=['POST', 'GET'])
+# def get_user_purchase_info():
+# 	#Get account IDs, customer names from customer (get customer by account ID), purchases on account, and the locations of the purchases from the merchant. money spent, etc.
+# 	account_id = request.form['account_id']
+# 	print account_id
+# 	Purchase = db.Purchase
+# 	a = list(Purchase.find({'payer_id':str(account_id)}))
+# 	for item in a:
+# 		doc = db.Merchant.find_one({'_id' : item['merchant_id']})
+# 		#print type(doc['geocode'])
+# 		if doc != None and 'geocode' in doc:
+# 			print  doc['geocode']
+# 			lat = doc['geocode']['lat']
+# 			lng = doc['geocode']['lng']
+# 			item['lat'] = lat
+# 			item['lng'] = lng
+# 			item['name'] = doc['name']
+
+	
+	# print a
+	# return dumps(a)
+
+# @app.route('/get_branches', methods=['GET'])
+# def get_branches():
+# 	Branch = db.Branch
+# 	cursor = Branch.find()
+# 	return dumps(cursor)
+
+def generate_JSON(workbook, type1):
+    worksheet = workbook.sheet_by_name('Data')
+    num_rows = worksheet.nrows
+    data = []
+    header = worksheet.row(0)
+    year_dict = {}
+    for i in range(7,header.__len__()):
+        cell = worksheet.cell_value(0, i)
+        length = len(cell)
+        type = cell[length - 1:]
+        if type == 'D':
+            year = cell[length-4:length-1]
+            year = 1800 + int(year)
+            year_dict[year] = i
+            # print year
+    temp = {}
+    curr_row = 1
+    while curr_row < (num_rows):
+        row = worksheet.row(curr_row)
+
+        county = row[0]
+        # temp1["County"] = str(county.value)
+        value = []
+        for year in year_dict:
+            # if year > 1999 and year < 2004:
+            val = str(row[int(year_dict[year])])
+            val = re.sub('number|:','',val)
+            if type1 == "Poverty":
+                val = float(val)/1000
+            if type1 == "Health":
+                val = float(val)*1000
+            value.append([str(year), val])
+
+        # data[curr_row] = {}
+        # temp2 = {}
+        # temp2[type] = ed_value
+        # data.append((temp1,temp2))
+        # data = data + "{"+'count : '+str(county) +", Edu_Value : " + ed_value + " },"
+        temp[str(county.value)] = value
+        curr_row += 1
+
+    # json_data = json.dumps(data)
+    return temp
+    # cPickle.dump(json_data, open("json_data.json", "w"))
+
+
+if __name__ == '__main__':
+    workbook_pov = xlrd.open_workbook("Poverty.xls")
+    poverty_data = generate_JSON(workbook_pov, "Poverty")
+    workbook_health = xlrd.open_workbook("Health.xls")
+    health_data = generate_JSON(workbook_health, "Health")
+    workbook_Edu = xlrd.open_workbook("Education.xls")
+    edu_data = generate_JSON(workbook_Edu, "Education")
+
+    data = []
+    i = 0
+    if poverty_data.__len__() == health_data.__len__() and health_data.__len__() == edu_data.__len__():
+        for county in poverty_data:
+            temp = {}
+            temp["name"] = str(county)
+            temp["region"] = str(county)
+            temp["population"] = health_data[county] # Circle
+            temp["lifeExpectancy"] = poverty_data[county] # y -axis
+            temp["income"] = edu_data[county] # x -axis
+            data.append(temp)
+            # if i > 0:
+            #     break
+            # break
+            # i += 1
+
+    simplejson.dump(data, open("Data.json", "w"))
+    # app.run(debug=True)
+
+
+
+
+
+
+
+    # worksheet = workbook.sheet_by_name('Data')
+    # num_rows = worksheet.nrows
+    # data = []
+    # header = worksheet.row(0)
+    # year_dict = {}
+    # for i in range(1,header.__len__()):
+    #     cell = worksheet.cell_value(0, i)
+    #     length = len(cell)
+    #     type = cell[length - 1:]
+    #     if type == 'D':
+    #         year = cell[length-4:length-1]
+    #         year = 1800 + int(year)
+    #         year_dict[year] = i
+    #         # print year
+    #
+    # curr_row = 1
+    # while curr_row < (num_rows):
+    #     row = worksheet.row(curr_row)
+    #     temp1 = {}
+    #     county = row[0]
+    #     temp1["County"] = str(county.value)
+    #     ed_value = []
+    #     for year in year_dict:
+    #         val = str(row[int(year_dict[year])])
+    #         ed_value.append([str(year),re.sub('number|:','',val)])
+    #
+    #     # data[curr_row] = {}
+    #     temp2 = {}
+    #     temp1["Heath_ValueS"] = ed_value
+    #     data.append((temp1))
+    #     # data = data + "{"+'count : '+str(county) +", Edu_Value : " + ed_value + " },"
+    #     curr_row += 1
+    #
+    # json_data = json.dumps(data)
